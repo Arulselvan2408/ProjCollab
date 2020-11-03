d5d1e513164bef5e659811bffd6957d66adcaa45
arulselvan2408 <barulselvan97@gmail.com>
2020-11-03 12:24:37 +0530

    Student Details

---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        static void Main()
+        {
+            string StudentName, SchoolName;
+            int Age;
+            Console.WriteLine("Enter the StudentName");
+            StudentName = Console.ReadLine();
+            Console.WriteLine("Enter the SchoolName");
+            SchoolName = Console.ReadLine();
+            Console.WriteLine("Enter the Age");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Name= {0}", StudentName);
+            Console.WriteLine("School= {0}", SchoolName);
+            Console.WriteLine("Age {0}", Age);
+
+            Console.Read();
+
+        }
+    }
+}
